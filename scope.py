331e990014b58c3f843587625596121123e8cca5
Simiel <87607921+simiel@users.noreply.github.com>
2022-02-26 18:19:39 +0000

    Lesson on scope

---
+++ scope.py
@@ -0,0 +1,16 @@
+spam = 50
+print(spam)
+
+
+def newFunction():
+    print('I am the function')
+    name = "Legon"
+    print(name)
+    # print(spam)
+    spam = 'Sam'
+    print(spam)
+
+
+newFunction()
+# print(name)
+print(spam)
